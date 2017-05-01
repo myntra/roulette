a58105ff3ef65666ba08b95a49cfe2f7c3cda963
Adnaan Badr <adnaan.badr@myntra.com>
2017-05-01 23:04:21 +0530

    add simple parser testcase

--- parser_test.go
+++ parser_test.go
@@ -1,5 +1,135 @@
 package roulette
 
-type testData struct {
-	in interface{}
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"testing"
+)
+
+// SimpleParseExpect ...
+type SimpleParseTestCase interface {
+	Parse()
+	Execute()
+	Expected() bool
+	Name() string
+	Desc() string
+}
+
+func readFile(path string) []byte {
+	ruleFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("ruleFile read err #%v  at path %v", err, path)
+	}
+
+	return ruleFile
+}
+
+// T common test object
+type T struct {
+	A           int
+	B           int
+	XML         string
+	Callback    func(val interface{})
+	Parser      *TextTemplateParser
+	Executor    *SimpleExecutor
+	ExpectFunc  func(val interface{}) bool
+	TestName    string
+	Description string
+}
+
+//SetA ...
+func (t *T) SetA(a int, prevVal ...bool) bool {
+	if len(prevVal) > 0 {
+		if !prevVal[0] {
+			return false
+		}
+	}
+	fmt.Println(a)
+	t.A = a
+	return true
+
+}
+
+// T1 ...
+type T1 struct {
+	*T
+}
+
+// Name ...
+func (t *T1) Name() string {
+	return t.TestName
+}
+
+// Desc ...
+func (t *T1) Desc() string {
+	return t.Description
+}
+
+// Execute ...
+func (t *T1) Execute() {
+	t.Executor.Execute(t)
+}
+
+// Parse ...
+func (t *T1) Parse() {
+
+	var parser Parser
+	var err error
+
+	if t.Callback != nil {
+		parser, err = NewCallbackParser(readFile(t.XML), t.Callback)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		parser, err = NewSimpleParser(readFile(t.XML))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	t.Parser = parser.(*TextTemplateParser)
+	t.Executor = NewSimpleExecutor(t.Parser).(*SimpleExecutor)
+}
+
+// Expected ...
+func (t *T1) Expected() bool {
+	return t.ExpectFunc(t)
+}
+
+var simpleParseTestCases = []SimpleParseTestCase{
+	&T1{
+		T: &T{
+			A:           1,
+			B:           2,
+			XML:         "testrules/rules_simple_1.xml",
+			TestName:    "TestT1SetA",
+			Description: "Expects T1.A to be 5",
+			ExpectFunc: func(val interface{}) bool {
+				t, ok := val.(*T1)
+				if !ok {
+					log.Println("expected val to be T1")
+					return false
+				}
+				if t.A != 5 {
+					return false
+				}
+				return true
+			}},
+	},
+}
+
+// test xml: tags and attributes, malformed etc.
+// test template: valid for types, malformed etc.
+// test parsers and executors: types, nil values etc.
+
+func TestSimpleParser(tt *testing.T) {
+	for _, testcase := range simpleParseTestCases {
+		testcase.Parse()
+		testcase.Execute()
+		if !testcase.Expected() {
+			log.Fatal(testcase.Name(), testcase.Desc())
+		}
+	}
 }
--- test/types.go
+++ test/types.go
@@ -1,7 +1,102 @@
 package test
 
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/myntra/roulette"
+)
+
+// SimpleParseExpect ...
+type SimpleParseExpect interface {
+	Expected() bool
+	Parse(fn func(val interface{}))
+	Execute()
+}
+
+func readFile(path string) []byte {
+	ruleFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("ruleFile read err #%v  at path %v", err, path)
+	}
+
+	return ruleFile
+}
+
+// T common test object
+type T struct {
+	A        int
+	B        int
+	XML      string
+	Parser   *roulette.TextTemplateParser
+	Executor *roulette.SimpleExecutor
+
+	Name        string
+	Description string
+}
+
+//SetA ...
+func (t *T) SetA(a int, prevVal ...bool) bool {
+
+	if len(prevVal) > 0 {
+		if !prevVal[0] {
+			return false
+		}
+	}
+	t.A = a
+	return true
+
+}
+
 // T1 ...
 type T1 struct {
-	A int
-	B int
+	T
+	ExpectFunc func(t *T1) bool
+}
+
+// Execute ...
+func (t *T1) Execute() {
+	t.Executor.Execute(t)
+}
+
+// Parse ...
+func (t *T1) Parse(fn func(val interface{})) {
+	parser, err := roulette.NewSimpleParser(readFile(t.XML))
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Parser = parser.(*roulette.TextTemplateParser)
+	t.Executor = roulette.NewSimpleExecutor(t.Parser).(*roulette.SimpleExecutor)
+}
+
+// Expected ...
+func (t *T1) Expected() bool {
+	return t.ExpectFunc(t)
+}
+
+// T2 ...
+type T2 struct {
+	T
+	ExpectFunc func(t *T2) bool
+}
+
+// Execute ...
+func (t *T2) Execute() {
+	t.Executor.Execute(t)
+}
+
+// Parse ...
+func (t *T2) Parse(fn func(val interface{})) {
+	parser, err := roulette.NewSimpleParser(readFile(t.XML))
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Parser = parser.(*roulette.TextTemplateParser)
+	t.Executor = roulette.NewSimpleExecutor(t.Parser).(*roulette.SimpleExecutor)
+
+}
+
+// Expected ...
+func (t *T2) Expected() bool {
+	return t.ExpectFunc(t)
 }
